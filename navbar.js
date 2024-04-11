679b6f2b0f0087c06c86b7469361695860486394
James Kettle <jameskettle2018@gmail.com>
2024-04-11 17:35:19 -0400

    remove nav bar

--- navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navbarToggle = document.getElementById('navbar-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
-  
-    navbarToggle.addEventListener('click', function () {
-      mobileMenu.classList.toggle('hidden');
-    });
-  });
